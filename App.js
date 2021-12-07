7469f6504170b28b183fe09a9b1e4a8e125d209d
yuvalpoliak <81253836+yuvalpoliak@users.noreply.github.com>
2021-12-07 16:57:43 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Platform, Button } from 'react-native';
+import { AdMobBanner, AdMobInterstitial } from 'expo-ads-admob';
 
-export default function App() {
+//ca-app-pub-1219874861745235/5176171062 android
+//ca-app-pub-1219874861745235/5876499905 ios
+
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.bannerAdId = Platform.OS === 'ios' ? 'ca-app-pub-3940256099942544/2934735716' : 
+  'ca-app-pub-3940256099942544/6300978111';
+    this.interstitialAdId = Platform.OS === 'ios' ? 'ca-app-pub-3940256099942544/4411468910'
+    : 'ca-app-pub-3940256099942544/1033173712'
+  }
+  async componentDidMount() {
+    AdMobInterstitial.setAdUnitID(this.interstitialAdId)
+    await AdMobInterstitial.requestAdAsync( { servePersonalizedAds: false })
+    await AdMobInterstitial.showAdAsync();
+  }
+
+  
+  render() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <AdMobBanner
+       bannerSize='banner'
+       adUnitID={this.bannerAdId}
+       servePersonalizedAds={false}
+
+        ></AdMobBanner>
+        
+        <AdMobBanner
+       bannerSize='smartBannerPortrait'
+       adUnitID={this.bannerAdId}
+       servePersonalizedAds={false}
+
+        ></AdMobBanner>
+        <AdMobBanner
+       bannerSize='mediumRectangle'
+       adUnitID={this.bannerAdId}
+       servePersonalizedAds={false}
+
+        ></AdMobBanner>
+        <AdMobBanner
+       bannerSize='smartBannerLandscape'
+       adUnitID={this.bannerAdId}
+       servePersonalizedAds={false}
+
+        ></AdMobBanner>
+        
+        
+        
+
+      
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
